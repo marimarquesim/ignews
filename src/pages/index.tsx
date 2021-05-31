868474f996268edb42ae1647de3b1716f6c3f478
= <=>
2021-05-30 21:42:12 -0300

    Formatação do preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import {GetStaticProps} from 'next';
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButon';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>  
@@ -18,9 +28,9 @@ export default function Home() {
         <h1>News about the <span>React</span> word.</h1>
         <p>
           Get acess to all the publictions <br/>
-          <span>for $9,90 month</span>
+          <span>for {product.amount} month</span>
         </p>
-        <SubscribeButton/>
+        <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="../images/avatar.svg" alt="Girl Coding"/>
 
@@ -28,3 +38,24 @@ export default function Home() {
     </>  
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+const price = await stripe.prices.retrieve('price_1IvWPpALrRdQyWjc06DDCKX2')
+
+const product = {
+  priceId: price.id,
+  amount: new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price.unit_amount / 100),
+};
+
+return {
+  props: {
+    product,
+  },
+  revalidate: 60 * 60 * 24, //24 hours
+}
+
+}
+
